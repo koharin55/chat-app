71dbacfece96f536f9dccb6b7f8b1f263330334c
koharin55 <ysugita0418@gmail.com>
2025-08-11 17:20:50 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create]
+end
+
+Rails.application.routes.draw do[]
   root to: "messages#index"
   devise_for :users
 end
